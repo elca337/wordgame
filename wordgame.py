8f62d9a0af40d48a72a2ebc1d6822bc59e639799
elca337 <leemcasey14@gmail.com>
2019-03-02 21:14:59 +0000

    Add files via upload

---
+++ wordgame.py
@@ -0,0 +1,7 @@
+verb = input("Please enter a verb:  ")
+noun = input("Please enter a noun:  ")
+adjective = input("Please enter an adjective:  ")
+
+print("I enjoy practice, I find it helps me to ", verb, "better.")
+print("Without practice my ", noun, "would probably not even work.")
+print("My code is getting more ", adjective, "every single day!")
